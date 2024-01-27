560b84afc521e51906ff9ff55a7ae15caccf0eb0
Ashutosh <Ashutosh@LAPTOP-N00785I6>
2024-01-27 21:02:54 +0530

    Redux Toolkit configuration complete

--- ideaHub/src/App.jsx
+++ ideaHub/src/App.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import { Header, Footer } from "./components/index.js";
+import authService from "./appwrite/auth.js";
+// import { Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
 function App() {
-  return <></>;
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400 justify-center">
+      <div className="w-full block">
+        <Header />
+        <main>{/* TODO:  <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
